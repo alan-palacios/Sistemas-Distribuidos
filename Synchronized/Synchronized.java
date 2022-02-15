71c60ed4383c3d659b407edd40df4ff277f8561b
AlanPL <apyolt@gmail.com>
2022-02-14 18:29:09 -0600

    Add Synchronized examples

---
+++ Synchronized/Synchronized.java
@@ -0,0 +1,36 @@
+/**
+ * SynchronizedExample
+ */
+public class Synchronized extends Thread{
+	static long n;
+	//Common static object to make possible the synchronization between the threads
+	static Object obj = new Object();
+	public void run() {
+		for (int i = 0; i < 100000; i++) {
+			//Synchronized instructions
+			synchronized (obj){
+				n++;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+		Synchronized t1 = new Synchronized();
+		Synchronized t2 = new Synchronized();
+		t1.start();
+		t2.start();
+		try {
+			t1.join();
+			t2.join();
+			System.out.println(n);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		long time = System.currentTimeMillis() - start;
+		//it takes more time in its execution 
+		//because the threads have to wait to each other to
+		//finish the synchronized instructions
+		System.out.println("Time: "+time);
+	}
+}
---
+++ Synchronized/Unsynchronized.java
@@ -0,0 +1,28 @@
+/**
+ * UnsynchronizedExample
+ */
+public class Unsynchronized extends Thread{
+	static long n;
+	public void run() {
+		for (int i = 0; i < 100000; i++) {
+			n++;
+		}
+	}
+
+	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+		Unsynchronized t1 = new Unsynchronized();
+		Unsynchronized t2 = new Unsynchronized();
+		t1.start();
+		t2.start();
+		try {
+			t1.join();
+			t2.join();
+			System.out.println(n);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		long time = System.currentTimeMillis() - start;
+		System.out.println("Time: "+time);
+	}
+}
