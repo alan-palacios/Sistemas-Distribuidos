226831c31dbc2046e1946c2db22089b6c08b1112
AlanPL <apyolt@gmail.com>
2022-02-20 22:50:55 -0600

    Add Jerarquia de Memoria

---
+++ JerarquiaMemoria/MultiplicacionMatrices_2.java
@@ -0,0 +1,44 @@
+package JerarquiaMemoria;
+
+/**
+ * MultiplicacionMatrices
+ */
+public class MultiplicacionMatrices_2 {
+	static int N = 1000;
+	static int[][] A = new int[N][N];
+	static int[][] B = new int[N][N];
+	static int[][] C = new int[N][N];
+
+	public static void main(String[] args) {
+		long t1 = System.currentTimeMillis();
+
+		//inicializar matrices A y B
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				A[i][j] = 2 * i - j;
+				B[i][j] = i + 2 * j;
+				C[i][j] = 0;
+			}
+		}
+
+		//se transpone la matriz b para optimizar las operaciones en memoria
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < i; j++) {
+				int x = B[i][j];
+				B[i][j] = B[j][i];
+				B[j][i] = x;
+			}
+		}
+		//multiplica 
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				for (int k = 0; k < N; k++) {
+					C[i][j] += A[i][k] * B[j][k];
+				}
+			}
+		}
+
+		long t2 = System.currentTimeMillis();
+		System.out.println("Tiempo: "+(t2-t1)+"ms");
+	}
+}
